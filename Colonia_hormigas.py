7b183770dfd191b2680738549a9d72d4fb8a66c1
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2022-10-17 10:47:08 -0300

    poblacion inicial mala

--- Colonia_hormigas.py
+++ Colonia_hormigas.py
@@ -1,7 +1,26 @@
 from pickle import NONE
 import numpy as np
 import pandas as pd
+import sys 
 
 
-df = pd.read_csv('berlin52.txt', sep =" ", header = None)
-print(df)
+# df = pd.read_csv('berlin52.txt', sep =" ", header = None)
+# print(df)
+
+if len(sys.argv) == 3:
+    seed = int(sys.argv[1])
+    n = int(sys.argv[2])
+    print(seed, n)
+else:
+    sys.exit(0)
+
+
+
+
+
+poblacion = np.zeros((n,52),int)
+for k in range(n):
+	poblacion[k]=np.arange(0,52)
+	np.random.shuffle(poblacion[k])
+print("Poblacion inicial :")
+print(poblacion)
