50c69a62beac323038036f50a5f0cb7f13ba6a23
DiegoAraneda98 <daranedah@ing.ucsc.cl>
2022-10-20 10:53:07 -0300

    actualización

--- Colonia_hormigas.py
+++ Colonia_hormigas.py
@@ -3,13 +3,11 @@ import pandas as pd
 import sys 
 
 
-df = pd.read_table('berlin52.tsp.txt', header = None,delim_whitespace=True, skiprows=6, skipfooter=2)
+df = pd.read_table('berlin52.tsp', header = None,delim_whitespace=True, skiprows=6, skipfooter=2, engine='python')
 df = df.drop(columns=0,axis=1).to_numpy()
 print(df)
-#variables = df.shape[0]
-#print(variables)
-# df = pd.read_csv('berlin52.txt', sep =" ", header = None)
-# print(df)
+variables = df.shape[0]
+print(variables)
 
 if len(sys.argv) == 3:
     seed = int(sys.argv[1])
@@ -24,3 +22,4 @@ for i in range(n):
     poblacion[i][0] = np.random.randint(52)
 print("Poblacion inicial :")
 print(poblacion)
+
