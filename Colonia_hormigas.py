db2ae64538da59d3c83a3fe62a9950d51ff3b37b
DiegoAraneda98 <daranedah@ing.ucsc.cl>
2022-10-20 09:35:12 -0300

    leer archivos

--- Colonia_hormigas.py
+++ Colonia_hormigas.py
@@ -3,5 +3,8 @@ import numpy as np
 import pandas as pd
 
 
-df = pd.read_csv('berlin52.txt', sep =" ", header = None)
+df = pd.read_table('berlin52.txt', header = None,delim_whitespace=True, skiprows=6, skipfooter=2)
+df = df.drop(columns =0,axis=1).to_numpy()
 print(df)
+variables = df.shape[0]
+print(variables)
