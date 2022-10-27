cd6d2afc671fd6900adf57948496e0bb843a17f6
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2022-10-26 22:43:53 -0300

    intento registrar visitados

--- Colonia_hormigas.py
+++ Colonia_hormigas.py
@@ -43,16 +43,15 @@ def Calcular_costo(n,s,c):
         aux += c[s[i]][s[i+1]]
     return aux
 
-def Valor_FeromonaxHeuristica(heuristica, matriz_feromona, memoria):
-    for k in range(h):
-        TxN = memoria[k]*matriz_feromona[poblacion[k][i]]*(heuristica[poblacion[k][i]]**importancia_heuristica)
+def Valor_FeromonaxHeuristica(heuristica, matriz_feromona, memoria,k):
+    TxN = memoria[k]*matriz_feromona[poblacion[k][i]]*(heuristica[poblacion[k][i]]**importancia_heuristica)
     return TxN
 
 
 coordenadas = pd.read_table(archivo, header = None,delim_whitespace=True, skiprows=6, skipfooter=2, engine='python')
 coordenadas = coordenadas.drop(columns =0,axis=1).to_numpy()
-print("Matriz Coordenadas: ")
-print(coordenadas)
+# print("Matriz Coordenadas: ")
+# print(coordenadas)
 cant_variables = coordenadas.shape[0]
 
 
@@ -62,13 +61,13 @@ for i in range(cant_variables-1):
     for j in range(i+1,cant_variables):
         distancias[i][j] = np.sqrt(np.sum(np.square(coordenadas[i]-coordenadas[j])))
         distancias[j][i] = distancias[i][j]
-print("Matriz Distancias: ")
-print(distancias)
+# print("Matriz Distancias: ")
+# print(distancias)
 
 heuristica = np.full_like(distancias,fill_value=1/distancias, dtype=float)
 np.fill_diagonal(heuristica,0)
-print("Matriz Heuristica: ")
-print(heuristica)
+# print("Matriz Heuristica: ")
+# print(heuristica)
 
 
 
@@ -77,16 +76,16 @@ print(heuristica)
 mejor_solucion = np.arange(0,cant_variables)
 np.random.shuffle(mejor_solucion)
 mejor_costo = Calcular_costo(cant_variables,mejor_solucion,distancias)
-print(mejor_costo)
+# print(mejor_costo)
 
 solucionMejorIteracion = 0
 
 Tij0=1/(cant_variables*mejor_costo)
+# print("Tij0: ", Tij0)
 matriz_feromona = np.full_like(distancias,fill_value=Tij0,dtype=float)
-print(matriz_feromona)
+# print("Matris feromona: ",matriz_feromona)
 
 
-lugares = np.arange(len(distancias))
 
 
 
@@ -96,15 +95,20 @@ while generacion < itereaciones and not (np.round(mejor_costo,decimals=4) == 754
     print('Generacion: ',generacion)
     poblacion = np.full((h, cant_variables), fill_value=-1, dtype=int)
     memoria = np.full_like(poblacion,1,dtype=int)
+    trayectoria = np.full_like(poblacion,1,dtype=int)
     for i in range(h):
         aux = np.random.randint(cant_variables)
         poblacion[i][0] = aux
         memoria[i][aux] = 0
     print("Poblacion inicial :")
     print(poblacion)
-    
+    print("tray: ", trayectoria)
     print("Memoria: ")
     print(memoria)
 
-       
-
+    for j in range(cant_variables):
+        for k in range(h):
+            TxN = Valor_FeromonaxHeuristica(heuristica,matriz_feromona,memoria,k)
+            index = np.where(TxN == np.amax(TxN))[0]
+            memoria[k][index] = 0
+            trayectoria[k][j] = index[0]
